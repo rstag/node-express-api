cbaf03ad74e3cbcccff9ede4b8255828b06438d9
Rohit <rohitsonawane181@gmail.com>
2018-10-09 17:06:46 +0530

    pgdb sql hello errs routers db

--- pgsql.js
+++ pgsql.js
@@ -42,42 +42,42 @@ const routers = express.Router();
 //     });
 // });
 
-routers.get('/create', (req, res) => {
+routers.get('/', (req, res) => {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) {
             res.send('Hello World error!'+err);
             console.log(err);
         }
-        conn.query('create table employees (id int,name varchar(255),salery varchar(255),address varchar(255))', function (err, result) {
-            done();
-            if (err) {
-                res.status(400).json({ error: err.message });
-            }
-            else {
-                // this will still cause jquery to display 'Record updated!'
-                // eventhough it was inserted
-                conn.query('insert into employees values(?,?,?,?);', [34, 'hgjg', 'cggh', 'hhh'], function (err, result) {
-                    done();
-                    if (err) {
-                        res.status(400).json({ error: err.message });
-                    }
-                    else {
-                        // this will still cause jquery to display 'Record updated!'
-                        // eventhough it was inserted
+    //     conn.query('create table employees (id int,name varchar(255),salery varchar(255),address varchar(255))', function (err, result) {
+    //         done();
+    //         if (err) {
+    //             res.status(400).json({ error: err.message });
+    //         }
+    //         else {
+    //             // this will still cause jquery to display 'Record updated!'
+    //             // eventhough it was inserted
+    //             conn.query('insert into employees values(?,?,?,?);', [34, 'hgjg', 'cggh', 'hhh'], function (err, result) {
+    //                 done();
+    //                 if (err) {
+    //                     res.status(400).json({ error: err.message });
+    //                 }
+    //                 else {
+    //                     // this will still cause jquery to display 'Record updated!'
+    //                     // eventhough it was inserted
 
 
 
-                        res.json(result);
-                    }
-                });
+    //                     res.json(result);
+    //                 }
+    //             });
 
 
-                res.json(result);
-            }
-        });
+    //             res.json(result);
+    //         }
+    //     });
     });
-})
+});
 
 // app.listen(app.get('port'), function () {
 //     console.log('Express server listening on port ' + app.get('port'));
